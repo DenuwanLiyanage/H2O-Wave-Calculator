1a0a59e4eb133ccf19e3c4a13673f2ff858e6386
Sachintha Denuwan Liyanage <48705649+DenuwanLiyanage@users.noreply.github.com>
2022-11-10 13:40:43 +0530

    Implement test case for complex arithmatic operations

--- Cal/Test_Calc.py
+++ Cal/Test_Calc.py
@@ -10,6 +10,10 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(Cal.perform_calculator("10*5"), 50)
         self.assertEqual(Cal.perform_calculator("10/5"), 2)
 
+    def test_perform_calculator_complex(self):
+        self.assertEqual(Cal.perform_calculator("10+5*2+4/2"), 22)
+        self.assertEqual(Cal.perform_calculator("10+16/4*2-3"), 15)
+
 
 if __name__ == '__main__':
     unittest.main()
