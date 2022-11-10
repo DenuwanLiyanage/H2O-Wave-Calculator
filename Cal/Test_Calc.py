a6b16535dd5c927d9c89de76d0337a854f6f447a
Sachintha Denuwan Liyanage <48705649+DenuwanLiyanage@users.noreply.github.com>
2022-11-10 13:20:35 +0530

    Implement test case for simple arithmatic operations

--- Cal/Test_Calc.py
+++ Cal/Test_Calc.py
@@ -4,8 +4,11 @@ import Cal
 
 class TestCalc(unittest.TestCase):
 
-    def test_perform_calculator(self):
+    def test_perform_calculator_simple(self):
         self.assertEqual(Cal.perform_calculator("10+5"), 15)
+        self.assertEqual(Cal.perform_calculator("10-5"), 5)
+        self.assertEqual(Cal.perform_calculator("10*5"), 50)
+        self.assertEqual(Cal.perform_calculator("10/5"), 2)
 
 
 if __name__ == '__main__':
